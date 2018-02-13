27f54bb2dbd0ff04628c04e837adcf5d8a5ec467
JoseV <josevega.eci@gmail.com>
2018-02-13 20:38:17 +0100

    Subido

---
+++ Assets/CounterOfDistance.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CounterOfDistance : MonoBehaviour {
+	public Text meters;
+	private float mult=0;
+	// Use this for initialization
+	void Start () {
+		meters.text="0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float multX = Input.acceleration.x * Input.acceleration.x;
+		float multY = Input.acceleration.y * Input.acceleration.y;
+		float multZ = Input.acceleration.z * Input.acceleration.z;
+		float sum = multX + multY + multZ;
+		float raiz = Mathf.Sqrt (sum);
+		float velocity = raiz-1f;
+		float rounded;
+		if (velocity > 0.1f && velocity < 0.6f) {
+			mult += velocity * 0.5f;
+			rounded = (float)(Math.Round ((double)mult, 2));
+			meters.text = rounded.ToString ("N");
+		} else if (velocity > 0.6) {
+			mult += 0.6f;
+			rounded = (float)(Math.Round ((double)mult, 2));
+			meters.text = rounded.ToString ("N");
+		}
+	}
+}
---
+++ Assets/CounterOfTime.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CounterOfTime : MonoBehaviour {
+//	public float timer;
+	public Text crono;
+	private float startTime;
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		timer += Time.deltaTime;
+		float timerControl = Time.time - startTime;
+		string mins = ((int)timerControl/60).ToString("00");
+		string segs = (timerControl % 60).ToString("00");
+		string milisegs = ((timerControl * 100)%100).ToString ("00");
+
+		string TimerString = string.Format ("{00}:{01}:{02}", mins, segs, milisegs);
+
+		crono.text = TimerString.ToString ();
+
+			
+	}
+
+//	void OnGUI() {
+//		int minutes = Mathf.FloorToInt(timer / 60F);
+//		int seconds = Mathf.FloorToInt(timer - minutes * 60);
+//		string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+//
+//		GUI.Label(new Rect(10,10,250,100), timer.ToString());
+//	}
+}
